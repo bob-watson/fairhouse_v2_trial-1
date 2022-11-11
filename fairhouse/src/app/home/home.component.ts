63d975eb8ef52a18ec6585f1c7d597d498a5324d
Bob Watson <bobwatson@google.com>
2022-11-11 10:41:14 -0800

    lesson-2-step-3-ng-generate-output

---
+++ fairhouse/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
