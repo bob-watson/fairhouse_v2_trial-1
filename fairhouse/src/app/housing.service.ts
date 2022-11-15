dfe8a6f7dd277079f27708273b5e318e03849ba7
Bob Watson <bobwatson@google.com>
2022-11-15 14:27:26 -0800

    lesson-9-step-3

--- fairhouse/src/app/housing.service.ts
+++ fairhouse/src/app/housing.service.ts
@@ -109,5 +109,13 @@ export class HousingService {
     }
   ];
 
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
   constructor() { }
 }
