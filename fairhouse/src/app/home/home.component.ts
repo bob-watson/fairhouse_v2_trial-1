86b52acbad351832e4d5792ca6e65fe404a3849a
Bob Watson <bobwatson@google.com>
2022-11-15 11:07:38 -0800

    lesson-8-step-2-rev

--- fairhouse/src/app/home/home.component.ts
+++ fairhouse/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ import { HousingLocation } from '../housing-location';
       <button type="button">Search</button>
     </section>
     <section class="results"></section>
-    <app-housing-location [housingLocation]="this.houseList[0]"></app-housing-location>
+    <app-housing-location [housingLocation]="this.houseList[1]"></app-housing-location>
   `,
   styles: [
   ]
@@ -36,6 +36,16 @@ export class HomeComponent implements OnInit {
         availableUnits: 99,
         wifi: true,
         laundry: false,
+      },
+      {
+        id: 8888,
+        name: "Test Home 2",
+        city: "Test city 2",
+        state: "ST-2",
+        photo: "photo URL 2 goes here",
+        availableUnits: 88,
+        wifi: false,
+        laundry: true,
       }
     ];
   }
