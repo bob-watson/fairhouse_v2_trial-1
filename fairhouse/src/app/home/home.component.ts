d8c55e96c8751b46d11df476f690ac5111bd5130
Bob Watson <bobwatson@google.com>
2022-11-15 15:37:40 -0800

    lesson-8-step-4

--- fairhouse/src/app/home/home.component.ts
+++ fairhouse/src/app/home/home.component.ts
@@ -23,6 +23,22 @@ import { HousingLocation } from '../housing-location';
     </section>
   `,
   styles: [
+    `input, button {
+      font-size: 18pt;
+      width: 60%;
+      padding: 8px;
+      border-radius: 10px;
+      border: solid .05em #4468e8
+    }`,
+    `button {
+      margin-left: .2em;
+      width: auto;
+    }`,
+    `.results {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 4em;
+    }`
   ]
 })
 export class HomeComponent implements OnInit {
