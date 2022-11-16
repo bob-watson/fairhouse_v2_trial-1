e0477af7fc6e200dc5fe4aebb16997f73c768dc9
Bob Watson <bobwatson@google.com>
2022-11-16 08:51:55 -0800

    lesson-10-step-3

--- fairhouse/src/app/app.routes.ts
+++ fairhouse/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 const appRoutes: Routes = [
   {
     path: '', component: HomeComponent,
+  },
+  {
+    path: 'details', component: DetailsComponent
   }
 ];
 
