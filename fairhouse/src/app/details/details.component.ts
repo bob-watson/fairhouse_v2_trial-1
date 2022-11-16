0f9841ecb838fa32a33c3110024fe9e45a730dd4
Bob Watson <bobwatson@google.com>
2022-11-16 10:57:43 -0800

    lesson-11-step-2

--- fairhouse/src/app/details/details.component.ts
+++ fairhouse/src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -11,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
       details works!
       ID  = {{this.housingLocationId}}
     </p>
+    <code> {{this.housingLocationDetails  | json }}</code>
   `,
   styles: [
   ]
@@ -18,6 +21,9 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent implements OnInit {
 
   private route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocationDetails: HousingLocation | undefined;
+
   housingLocationId: number = 0;
 
   constructor() { }
@@ -25,6 +31,8 @@ export class DetailsComponent implements OnInit {
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     this.housingLocationId = Number(routeParams.get('id'));
+
+    this.housingLocationDetails = this.housingService.getHousingLocationById(this.housingLocationId);
   }
 
 }
