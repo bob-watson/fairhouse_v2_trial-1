71015c04e87258152e0662385fe407559e6d1fec
Bob Watson <bobwatson@google.com>
2022-11-16 08:27:38 -0800

    lesson-10-step-1

---
+++ fairhouse/src/app/details/details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
