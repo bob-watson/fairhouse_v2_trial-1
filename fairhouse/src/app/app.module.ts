02508f273692c594c1ca7dccc8acccd29ac3225d
Bob Watson <bobwatson@google.com>
2022-11-16 08:42:03 -0800

    lesson-10-step-2

--- fairhouse/src/app/app.module.ts
+++ fairhouse/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+
+import appRoutes from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,10 @@ import { HousingLocationComponent } from './housing-location/housing-location.co
   imports: [
     BrowserModule,
     HomeComponent,
-    HousingLocationComponent
+    HousingLocationComponent,
+    RouterModule
   ],
-  providers: [],
+  providers: [provideRouter(appRoutes)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
