9e37418cc74950d9d9b8a39554cacd2be065c4ff
Bob Watson <bobwatson@google.com>
2022-11-17 08:36:44 -0800

    lesson-12-step-2

--- fairhouse/src/app/housing.service.ts
+++ fairhouse/src/app/housing.service.ts
@@ -117,5 +117,9 @@ export class HousingService {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Fairhouse application recieved: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
   constructor() { }
 }
