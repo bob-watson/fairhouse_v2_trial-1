9a884a26615d6507a9c9f05af9f3bcc2a9a5b39d
Bob Watson <bobwatson@google.com>
2022-11-17 09:20:07 -0800

    lesson-12-step-4

--- fairhouse/src/app/details/details.component.ts
+++ fairhouse/src/app/details/details.component.ts
@@ -46,25 +46,62 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   </section>
   `,
   styles: [
-    `.listing {
-      margin: .5em 2em 4em 0;
-    }`,
-    `.listing-heading {
-      font-size: 18pt;
-      width: 98%;
-      height: 60px;
-    }`,
-
-    `.listing-location {
-      color: gray;
-    }`,
-
-    `.listing-photo {
-      height: 300px;
-      width: 300px;
-      object-fit: cover;
-      border-radius: 10px;
-    }`
+    `
+      article.listing-details {
+        max-width: 1280px
+      }
+      ul > li {
+        font-size: 16pt;
+        margin: .7em 0 .7em 0;
+        color: rgb(113, 113, 113);
+      }
+      .listing-features {
+        flex-basis: 100%;
+      }
+      .listing-description {
+        display: flex;
+        float: left;
+      }
+      .section-heading {
+        font-size: 22pt;
+        flex-basis: 100%;
+        margin-bottom: .5em;
+      }
+      .listing-heading {
+        font-size: 32pt;
+        margin: .5em 0 .5em 0;
+      }
+      .listing-photo {
+        height: 300px;
+        width: 300px;
+        object-fit: cover;
+        border-radius: 10px;
+      }
+      .listing-apply {
+        background: white;
+        padding: 1em;
+        border: 1px solid rgb(221, 221, 221);
+        box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
+        border-radius: 10px;
+        width: 100%;
+        height: fit-content;
+        max-width: 40em;
+      }
+      form {
+        display: flex;
+        flex-direction: column;
+        row-gap: 0.5em;
+      }
+      label, input {
+        display: block;
+      }
+      input[type="text"], input[type="email"] {
+        font-size: 14pt;
+        border: 1px solid rgb(221, 221, 221);
+        border-radius: .1em;
+        padding: 10px;
+      }
+    `
   ]
 })
 export class DetailsComponent implements OnInit {
